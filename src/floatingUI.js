62bf413a8680c670ee8670ae6a8722958fe82349
Rohini Deshmukh <r.deshmukh001@umb.edu>
2024-07-20 19:50:22 -0400

    urls

--- src/floatingUI.js
+++ src/floatingUI.js
@@ -1,7 +1,7 @@
 var script = document.createElement("script");
 script.type = "text/javascript";
 // script.src = "http://localhost:8000/dist/powerboost.min.js";
-script.src = "https://github.com/RohiniDeshmukh/powerboost/blob/main/dist/powerboost.min.js";
+script.src = "https://raw.githubusercontent.com/RohiniDeshmukh/powerboost/main/dist/powerboost.min.js";
 document.head.appendChild(script);
 
 
