6282b6e39e23607e816e4df7b5fdbda06c4bbd42
Rohini Deshmukh <r.deshmukh001@umb.edu>
2024-07-27 10:58:15 -0400

    framework.name added

--- src/powerboost.js
+++ src/powerboost.js
@@ -6,8 +6,8 @@ export class Powerboost {
   }
 
   load_html(callback) {
-    // Util.load_Html("https://rohinideshmukh.github.io/powerboost/src/UI_components/index.html");
-    Util.load_Html("http://localhost:8000/src/UI_components/index.html");
+    Util.load_Html("https://rohinideshmukh.github.io/powerboost/src/UI_components/index.html");
+    // Util.load_Html("http://localhost:8000/src/UI_components/index.html");
 
     console.log("HTML loaded.");
     callback();
@@ -18,15 +18,15 @@ export class Powerboost {
   load_urls(callback) {
     console.log("inside load_links")
     Util.load_Links("stylesheet", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css");
-    // Util.load_Links("stylesheet", "https://rohinideshmukh.github.io/powerboost/src/UI_components/style.css");
-    Util.load_Links("stylesheet", "http://localhost:8000/src/UI_components/style.css");
+    Util.load_Links("stylesheet", "https://rohinideshmukh.github.io/powerboost/src/UI_components/style.css");
+    // Util.load_Links("stylesheet", "http://localhost:8000/src/UI_components/style.css");
 
     console.log("Loading scripts...");
     Util.load_Scripts("https://rohinideshmukh.github.io/boostlet/dist/boostlet.min.js");
     Util.load_Scripts("https://cdnjs.cloudflare.com/ajax/libs/ace/1.33.0/ace.js");
 
-      console.log("Scripts loaded.");
-      // callback();
+    console.log("Scripts loaded.");
+    // callback();
 
   }
 
